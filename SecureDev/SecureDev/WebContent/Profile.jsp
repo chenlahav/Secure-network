3debe1aa514696f049a76edcc201885ac95c472d
hillaitzhak <31000908+hillaitzhak@users.noreply.github.com>
2017-09-04 21:05:35 +0300

    test

--- SecureDev/SecureDev/WebContent/Profile.jsp
+++ SecureDev/SecureDev/WebContent/Profile.jsp
@@ -14,11 +14,12 @@
 html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
 </style>
 <body>
+<form action="ProfileController" method="post">
 <body class="w3-theme-l5">
 <!-- Profile -->
   <div class="w3-card-2 w3-round w3-white">
     <div class="w3-container">
-    <h4 class="w3-center">${requestScope['user'].username}</h4>
+    <h4 class="w3-center">${SessionScoped['user'].newUser}</h4>
     <p class="w3-center"><img src="/w3images/avatar3.png" class="w3-circle" style="height:106px;width:106px" alt="Avatar"></p>
     <hr>
     <p><i class="fa fa-pencil fa-fw w3-margin-right w3-text-theme"></i> Designer, UI</p>
@@ -26,5 +27,7 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
     <p><i class="fa fa-birthday-cake fa-fw w3-margin-right w3-text-theme"></i> April 1, 1988</p>
     </div>
 </div>
+</body>
+</form>
 </body>
 </html>
--- SecureDev/SecureDev/src/Repository/UserRepository.java
+++ SecureDev/SecureDev/src/Repository/UserRepository.java
@@ -1,6 +1,7 @@
 package Repository;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import Repository.AbstractRepository;
 
@@ -8,6 +9,10 @@ import model.User;
 
 public class UserRepository extends AbstractRepository{
 	
+	public UserRepository() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	public String addUser(User newUser){
 		
 		Connection c = this.connectionToDB();
@@ -31,7 +36,40 @@ public class UserRepository extends AbstractRepository{
 
 		else
 		{
-			return "SQL ERROR";
-		}
+			return "Connection ERROR";
 		}
 	}
+	public User getUser(String id){
+		Connection c = this.connectionToDB();
+		if (c!=null){
+			try{
+				Statement stmt = null;
+				stmt = c.createStatement();
+				String sql="SELECT * FROM tblusers WHERE id = "+id+";";
+				ResultSet rs = stmt.executeQuery(sql);
+				if (rs.next()) {
+					User userRequested = null;
+					userRequested.setId(id);
+					userRequested.setUsername(rs.getString("username"));
+					userRequested.setFirstName(rs.getString("firstName"));
+					userRequested.setLastName(rs.getString("lastName"));
+					userRequested.setEmail(rs.getString("email"));
+					userRequested.setBday(rs.getString("birthOfDate"));
+					userRequested.setGender(rs.getString("gnder"));
+					return userRequested;
+				  } 
+				else
+				  {
+					return null;
+				  }
+			   }catch (Exception e){
+				   return null;
+			   }
+			}
+
+		else
+		{
+			return null;
+		}
+	}
+}
