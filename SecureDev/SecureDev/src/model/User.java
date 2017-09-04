001bb925128cb67766658119726f5e1cdb2c47e2
chen lahav <chenlahav6@gmail.com>
2017-09-04 21:04:12 +0300

    Revert "Repository"
    This reverts commit 1ca147b72bfd87a8ad760e400197bd65f157080c.

--- SecureDev/SecureDev/src/model/User.java
+++ SecureDev/SecureDev/src/model/User.java
@@ -1,8 +1,6 @@
 package model;
 
 import java.io.IOException;
-import java.sql.Date;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,63 +16,7 @@ public class User {
 	 
 	private String username;
 	private String password;
-	private int id;
-	private String email;
-	private String firstName;
-	private String lastName;
-	private Date Bday;
-	private String gender;
-	
-	
  
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public Date getBday() {
-		return Bday;
-	}
-
-	public void setBday(Date bday) {
-		Bday = bday;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
 	public User(String username, String password){
 		this.username = username;
 		this.password = password;
